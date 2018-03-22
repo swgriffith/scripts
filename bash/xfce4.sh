86e8aa2b701916e8e949fa33582217ea66283d05
Steve Griffith <steve.griffith@microsoft.com>
2018-03-21 21:19:02 -0400

    Added comment for how to start the xrdp server

--- bash/xfce4.sh
+++ bash/xfce4.sh
@@ -6,3 +6,5 @@ apt-get --yes --force-yes install lubuntu-desktop xorg xrdp
 echo "[+] Configuring XRDP to listen on port 3390 (but not starting the service)..."
 sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
 
+# sudo /etc/init.d/xrdp start
+
