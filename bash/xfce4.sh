b99b3ba00d89fd0cb8ca0692469ed462c33785fa
Steve Griffith <steve.griffith@microsoft.com>
2018-03-21 20:56:18 -0400

    Added new xfce4 setup script

---
+++ bash/xfce4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "[+] Installing XFCE4, this will take a while"
+apt-get update
+apt-get dist-upgrade -y --force-yes
+apt-get --yes --force-yes install lubuntu-desktop xorg xrdp
+echo "[+] Configuring XRDP to listen on port 3390 (but not starting the service)..."
+sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
+
