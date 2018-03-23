a6326b52be729584c230d56d3ac6e411896ee1d5
Steve Griffith <stgriffi@microsoft.com>
2018-03-23 09:21:28 -0400

    Update xfce4.sh

--- bash/xfce4.sh
+++ bash/xfce4.sh
@@ -2,7 +2,7 @@
 echo "[+] Installing XFCE4, this will take a while"
 apt-get update
 apt-get dist-upgrade -y --force-yes
-apt-get --yes --force-yes install xubuntu-package xorg xrdp
+apt-get --yes --force-yes install xfce4 xorg xrdp
 echo "[+] Configuring XRDP to listen on port 3390 (but not starting the service)..."
 sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
 
